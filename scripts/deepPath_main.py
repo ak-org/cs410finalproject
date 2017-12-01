5f9af429350badf93c64e4945d0779cc81705cad
Ashish Kumar <ak-org@users.noreply.github.com>
2017-11-30 20:57:13 -0800

    V 0.2

--- scripts/deepPath_main.py
+++ scripts/deepPath_main.py
@@ -14,15 +14,14 @@ import sys
 import time
 
 import numpy as np
+import tensorforce
 
 from env import Env # this is the original DeepPath Env class
 from utils import *
 ## import necessary tensorflow classes
 
 from tensorforce import TensorForceError
-from tensorforce.agents import agents
-from tensorforce.core.networks import from_json
-from tensorforce.config import Configuration
+from tensorforce.agents import VPGAgent
 from tensorforce.execution import Runner
 from tensorforce.agents import agents
 from Tforcedp import DPEnv
@@ -31,38 +30,15 @@ def main():
 
     parser = argparse.ArgumentParser()
     parser.add_argument('-r', '--relation', help="Number of episodes")
-    parser.add_argument('-a', '--agent', default='VPGAgent')
-    parser.add_argument('-c', '--agent-config', default = './args/deepPath_agent_config.json', help="Agent configuration file")
-    parser.add_argument('-n', '--network-config', default = './args/deepPath_agent_network.json', help="Network configuration file")
     parser.add_argument('-e', '--episodes', type=int, default=500, help="Number of episodes")
-    parser.add_argument('-D', '--debug', action='store_true', default=False, help="Show debug outputs")
-    parser.add_argument('-s', '--save', default = './DPAgents', help="Save agent to this dir (default ./DPAgents)")
-    parser.add_argument('-se', '--save-episodes', type=int, default=100, help="Save agent every x episodes")
-
-
-    """"
-    Add support for following arguments after training is successful
-
-    #parser.add_argument('-l', '--load', help="Load agent from this dir")
-    """
+    #parser.add_argument('-s', '--save', default = './DPAgents', help="Save agent to this dir (default ./DPAgents)")
+    #parser.add_argument('-se', '--save-episodes', type=int, default=100, help="Save agent every x episodes")
 
     args = parser.parse_args()
 
     logger = logging.getLogger(__name__)
     logger.setLevel(logging.DEBUG) #can be changed
 
-    ## Parse for following Arguments
-    ##
-    ## relation
-    ## agent
-    ## agent Configuration JSON file
-    ## network configuration JSON file
-    ## number of episodes - default to 500
-    ##
-
-    ## if relation name is not provided  as an input Parameter
-    ## throw an Exception
-    ## rest of the input parameters have the default value
 
     if args.relation: # relation is defined
        relation = args.relation
@@ -70,68 +46,37 @@ def main():
         logger.error("Error : No Relation name provided!")
         return
 
-    if args.network_config:
-        ## define network
-       network = Configuration_from_json(args.network_config)
-    else:
-        logger.error("Error : No Network Config JSON file specified!")
-        return
 
-
-    if args.agent_config:
-       agent_config = Configuration_from_json(args.agent_config)
-       ## define agent using agent_config
-       ## takes a dictionary containing variables for
-       ## states, actions and network specs.
-        agent = Agent.from_spec(
-                        spec=agent_config,
-                        kwargs=dict(
-                            states_spec=environment.states, # TODO
-                            actions_spec=environment.actions, # TODO
-                            network_spec=network_spec # TODO
-                        )
-    )
-    else:
-        logger.error("Error : No Agent Config JSON file specified!")
-        return
-
-
-    graphpath = dataPath + 'tasks/' + relation + '/' + 'graph.txt'
+    graphPath = dataPath + 'tasks/' + relation + '/' + 'graph.txt'
     relationPath = dataPath + 'tasks/' + relation + '/' + 'train_pos'
 
-    if args.debug:
-        logger.info('-' * 16)
-        logginer.info("Parameter")
-        logger.info(relation)
-        logger.info(agent_config)
-        logger.info(network_config)
-        logger.info(graphPath)
-        logger.info(relationPath)
-
-
-    if args.save:
-    save_dir = os.path.dirname(args.save)
-    if not os.path.isdir(save_dir):
-        try:
-            os.mkdir(save_dir, 0o755)
-        except OSError:
-            raise OSError("Error Saving Agent info in dir {} ()".format(save_dir))
 
     ## initialize the DeePath Environment class
     environment = DPEnv(graphPath, relationPath)
 
-    ## define runner
-    ## uncomment once the agent,network and env details are hashed out
-    """
+    network_spec = [
+         ## need more clarification
+         dict(type='dense', size=512, activation='relu'),
+         dict(type='dense', size=1024 , activation='relu'),
+         dict(type='dense', size=state_dim , activation='softmax'),
+
+    ]
+
+    step_optimizer = dict(
+                     type='adam',
+                     learning_rate = 1e-3)
+
+    agent = VPGAgent(states_spec = environment.states, actions_spec = environment.actions,
+                     network_spec = network_spec, optimizer=step_optimizer,
+                     discount=0.99, batch_size=1000)
+
     runner = Runner(agent=agent,
                     environment = environment,
                     repeat_actions = 1,
-                    save_path = args.save,
-                    save_episodes = args.save_episodes)
+                    save_episodes = 100)
 
-    report_episodes = args.episodes/50 // default episodes = 500
-    if args.debug
-        report_episodes = 1
+    report_episodes = args.episodes/50 # default episodes = 500
+
 
     def episode_finished(r):
         if r.episode % report_episodes == 0:
@@ -146,11 +91,8 @@ def main():
     logger.info("Starting {agent} for Environment '{env}'".format(agent=agent, env=environment))
     runner.run(args.episodes, args.max_timesteps, episode_finished=episode_finished)
     logger.info("Learning finished. Total episodes: {ep}".format(ep=runner.episode))
-
-    """
-
     environment.close()
 
 
-if __name__ == '__main__'
+if __name__ == '__main__':
     main()
