9d922cc656ba02eebc131ce1e944858df5d789fd
arunarn2 <arunarn2@illinois.edu>
2017-12-02 14:42:16 -0800

    Add files via upload

---
+++ scripts/Tforcedp.py
@@ -0,0 +1,133 @@
+import numpy as np
+
+from env import Env
+from utils import *
+import numpy as np
+import random
+from tensorforce.environments.environment import Environment
+
+'''
+This file contains wrapper deeppath environment class
+mapped to Tensorforce's Environment class
+It will be called back in the deepPath_main.py file
+
+'''
+
+
+class DPEnv(Environment):
+    def __init__(self, relationPath, graphPath, task=None):
+        self.graphPath = graphPath
+        self.relationPath = relationPath
+        f1 = open(dataPath + 'entity2id.txt')
+        f2 = open(dataPath + 'relation2id.txt')
+        self.entity2id = f1.readlines()
+        self.relation2id = f2.readlines()
+        f1.close()
+        f2.close()
+        self.entity2id_ = {}
+        self.relation2id_ = {}
+        self.relations = []
+        for line in self.entity2id:
+            self.entity2id_[line.split()[0]] = int(line.split()[1])
+        for line in self.relation2id:
+            self.relation2id_[line.split()[0]] = int(line.split()[1])
+            self.relations.append(line.split()[0])
+        self.entity2vec = np.loadtxt(dataPath + 'entity2vec.bern')
+        self.relation2vec = np.loadtxt(dataPath + 'relation2vec.bern')
+
+        self.path = []
+        self.path_relations = []
+
+        # Knowledge Graph for path finding
+        f = open(dataPath + 'kb_env_rl.txt')
+        kb_all = f.readlines()
+        f.close()
+
+        self.kb = []
+        if task != None:
+            relation = task.split()[2]
+            for line in kb_all:
+                rel = line.split()[2]
+                if rel != relation and rel != relation + '_inv':
+                    self.kb.append(line)
+
+        self.die = 0  # record how many times does the agent choose an invalid path
+
+
+    def __str__(self):
+        return 'DeepPath Env({}:{})'.format(self.relationPath, self.graphPath)
+
+    def close(self):
+        self.env = None
+
+    def reset(self):
+        self.entity2id = {}
+        self.relation2id = {}
+        self.relations = {}
+        self.entity2id_ = {}
+        self.relation2id_ = {}
+        self.relations_ = {}
+        #self.entity2vec = np.empty()
+        #self.relation2vec = np.empty()
+        self.die = 0  # same as in init state
+
+    def execute(self, state, action):
+        '''
+        This function process the interact from the agent
+        state: is [current_position, target_position]
+        action: an integer
+        return: ([new_postion, target_position], reward, done)
+        '''
+        done = 0  # Whether the episode has finished
+        curr_pos = state[0]
+        target_pos = state[1]
+        chosed_relation = self.relations[action]
+        choices = []
+        for line in self.kb:
+            triple = line.rsplit()
+            e1_idx = self.entity2id_[triple[0]]
+
+            if curr_pos == e1_idx and triple[2] == chosed_relation and triple[1] in self.entity2id_:
+                choices.append(triple)
+        if len(choices) == 0:
+            reward = -1
+            self.die += 1
+            next_state = state  # stay in the initial state
+            next_state[-1] = self.die
+            return (next_state, reward, done)
+        else:  # find a valid step
+            path = random.choice(choices)
+            self.path.append(path[2] + ' -> ' + path[1])
+            self.path_relations.append(path[2])
+            # print 'Find a valid step', path
+            # print 'Action index', action
+            self.die = 0
+            new_pos = self.entity2id_[path[1]]
+            reward = 0
+            new_state = [new_pos, target_pos, self.die]
+
+            if new_pos == target_pos:
+                print 'Find a path:', self.path
+                done = 1
+                reward = 0
+                new_state = None
+            return (new_state, reward, done)
+
+    def states(self, idx_list=None):
+        if idx_list != None:
+            curr = self.entity2vec[idx_list[0], :]
+            targ = self.entity2vec[idx_list[1], :]
+            # return (np.expand_dims(np.concatenate((curr, targ - curr)),axis=0)
+            return dict(shape=(1, state_dim), type='float')
+        else:
+            return dict(shape=(), type='float')
+
+    def actions(self, entityID=0):
+        actions = set()
+        for line in self.kb:
+            triple = line.split()
+            e1_idx = self.entity2id_[triple[0]]
+            if e1_idx == entityID:
+                actions.add(self.relation2id_[triple[2]])
+                # return np.array(list(actions))
+        return dict(shape=400, type='int', num_actions=action_space)
---
+++ scripts/deepPath_main.py
@@ -0,0 +1,94 @@
+"""
+DeepPath main function
+based on Tensorforce
+"""
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import argparse
+import logging
+import os
+import sys
+import time
+
+import numpy as np
+import tensorforce
+
+from env import Env  # this is the original DeepPath Env class
+from utils import *
+## import necessary tensorflow classes
+
+from tensorforce import TensorForceError
+from tensorforce.agents import VPGAgent
+from tensorforce.agents import DQNAgent
+from tensorforce.execution import Runner
+from tensorforce.agents import agents
+from Tforcedp import DPEnv
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-r', '--relation', help="Number of episodes")
+    parser.add_argument('-e', '--episodes', type=int, default=500, help="Number of episodes")
+    # parser.add_argument('-s', '--save', default = './DPAgents', help="Save agent to this dir (default ./DPAgents)")
+    # parser.add_argument('-se', '--save-episodes', type=int, default=100, help="Save agent every x episodes")
+
+    args = parser.parse_args()
+
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.DEBUG)  # can be changed
+
+    if args.relation:  # relation is defined
+        relation = args.relation
+    else:
+        logger.error("Error : No Relation name provided!")
+        return
+
+    graphPath = dataPath + 'tasks/' + relation + '/' + 'graph.txt'
+    relationPath = dataPath + 'tasks/' + relation + '/' + 'train_pos'
+
+    ## initialize the DeePath Environment class
+    environment = DPEnv(graphPath, relationPath)
+
+    network_spec = [
+        dict(type='dense', size=512, activation='relu'),
+        dict(type='dense', size=1024, activation='relu'),
+
+    ]
+
+    step_optimizer = dict(type='adam', learning_rate=1e-3)
+
+    agent = VPGAgent(states_spec=dict(shape=(1, state_dim), type='float'),
+                     actions_spec=dict(num_actions=action_space, type='int'),
+                     network_spec=network_spec, optimizer=step_optimizer,
+                     discount=0.99, batch_size=1000)
+    # agent = DQNAgent(states_spec=dict(shape=(1, state_dim), type='float'),
+    #                  actions_spec=dict(num_actions=action_space, type='int'),
+    #                  network_spec=network_spec, optimizer=step_optimizer,
+    #                  discount=0.99, batch_size=1000)
+
+    runner = Runner(agent=agent, environment=environment)
+
+    report_episodes = args.episodes / 50  # default episodes = 500
+
+    def episode_finished(r):
+        if r.episode % report_episodes == 0:
+            sps = r.total_timesteps / (time.time() - r.start_time)
+            logger.info(
+                "Finished episode {ep} after {ts} timesteps. Steps Per Second {sps}".format(ep=r.episode, ts=r.timestep,
+                                                                                            sps=sps))
+            logger.info("Episode reward: {}".format(r.episode_rewards[-1]))
+            logger.info("Average of last 50 rewards: {}".format(sum(r.episode_rewards[-50:]) / 50))
+            logger.info("Average of last 100 rewards: {}".format(sum(r.episode_rewards[-100:]) / 100))
+        return True
+
+    logger.info("Starting {agent} for Environment '{env}'".format(agent=agent, env=environment))
+    runner.run(args.episodes, 50, episode_finished=episode_finished)
+    logger.info("Learning finished. Total episodes: {ep}".format(ep=runner.episode))
+    environment.close()
+
+
+if __name__ == '__main__':
+    main()
